d3c380c8dc7c3c783f9a5b713c6018cdb8266782
Anıl Başaran <nlbsrn@gmail.com>
2024-09-22 20:56:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HeroSection from './HeroSection'
+import './App.css'
+
+const  App = () => {
+
+
+  return (
+    
+    <div className='container font-robot flex justify-center' >
+
+     <HeroSection />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/HeroSection.jsx
@@ -0,0 +1,29 @@
+const HeroSection =()=>{
+    return (
+        <div className="flex justify-center" >
+
+            <div className="w-full max-w-5xl flex flex-col-reverse md:flex-row px-4 justify-center items-center border border-rose-500 py-32" >
+
+            
+                <div className="text-violet-800 text-4xl md:text-5xl font-ubuntu font-medium max-w-[450px]">
+                    
+
+                    Something
+                    <span className="text-yellow-600"> Catchy</span> and{" "} <span className="text-red-600"> Technological</span>
+                    <button
+                className="py-3 px-10 border-4 font-medium border-violet-800 text-xl mt-4">Learn More</button>
+                    </div>
+
+
+                <div>
+                
+                    <img
+                    className="md:w-[450px] my-16"
+                     src="https://static-task-assets.react-formula.com/963190.png" />
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+export default HeroSection;
